8b3ef03d16bd23b0fa45873426ab127437f3d84c
Renato Rosa Franco <Renato_ny@live.com>
2024-01-12 12:34:05 -0300

    disabling minio.

--- scripts/minio.sh
+++ scripts/minio.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Start MinIO server in background
-minio server /data &
+# # Start MinIO server in background
+# minio server /data &
 
-# Wait for the MinIO server to be up and running
-while ! nc -Z localhost 9000; do
-  sleep 1
-done
+# # Wait for the MinIO server to be up and running
+# while ! nc -Z localhost 9000; do
+#   sleep 1
+# done
 
-# Configure MinIO Client with endpoint, access key, and secret key
-mc alias set myminio http://localhost:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+# # Configure MinIO Client with endpoint, access key, and secret key
+# mc alias set myminio http://localhost:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 
-# Create the bucket if it doesn't exist
-mc mb myminio/rubypolska_development_bucket
+# # Create the bucket if it doesn't exist
+# mc mb myminio/rubypolska_development_bucket
 
-# Keep the container running
-wait
+# # Keep the container running
+# wait
